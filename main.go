8724afd33a0a66d7844cafbd10f383ea48e85a94
faygun <faygun89@gmail.com>
2020-10-14 19:18:54 +0100

    Init method was added for sample data.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -12,6 +14,44 @@ type Product struct {
 	PricePerUnit   string `json:"pricePerUnit"`
 	QuantityOnHand int    `json:"quantityOnHand"`
 	ProductName    string `json:"productName"`
+}
+
+var productList []Product
+
+func init() {
+	productListJSON := `[
+		{
+		  "productId": 1,
+		  "manufacturer": "Johns-Jenkins",
+		  "sku": "p5z343vdS",
+		  "upc": "939581000000",
+		  "pricePerUnit": "497.45",
+		  "quantityOnHand": 9703,
+		  "productName": "sticky note"
+		},
+		{
+		  "productId": 2,
+		  "manufacturer": "Hessel, Schimmel and Feeney",
+		  "sku": "i7v300kmx",
+		  "upc": "740979000000",
+		  "pricePerUnit": "282.29",
+		  "quantityOnHand": 9217,
+		  "productName": "leg warmers"
+		},
+		{
+		  "productId": 3,
+		  "manufacturer": "Swaniawski, Bartoletti and Bruen",
+		  "sku": "q0L657ys7",
+		  "upc": "111730000000",
+		  "pricePerUnit": "436.26",
+		  "quantityOnHand": 5905,
+		  "productName": "lamp shade"
+		}]`
+
+	err := json.Unmarshal([]byte(productListJSON), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
