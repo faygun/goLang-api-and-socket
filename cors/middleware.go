1faa694082befd7d13d65ee1af9c84e9e7108100
faygun <faygun89@gmail.com>
2020-10-15 20:55:14 +0100

    Middleware was seperated

---
+++ cors/middleware.go
@@ -0,0 +1,16 @@
+package cors
+
+import (
+	"net/http"
+)
+
+// Middleware :
+func Middleware(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		handler.ServeHTTP(w, r)
+	})
+}
