423cdc97b8fd85863c917a9b90797a568ddd1c93
faygun <faygun89@gmail.com>
2020-10-15 20:55:59 +0100

    The main class was tidied up

--- main.go
+++ main.go
@@ -1,180 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
-	"strings"
+
+	"github.com/faygun/goLang-api-and-socket/product"
 )
 
-type Product struct {
-	ProductID      int    `json:"productId"`
-	Manufacturer   string `json:"manufacturer"`
-	Sku            string `json:"sku"`
-	Upc            string `json:"upc"`
-	PricePerUnit   string `json:"pricePerUnit"`
-	QuantityOnHand int    `json:"quantityOnHand"`
-	ProductName    string `json:"productName"`
-}
+const basePath = "/api"
 
-var productList []Product
-
-func init() {
-	productListJSON := `[
-		{
-		  "productId": 1,
-		  "manufacturer": "Johns-Jenkins",
-		  "sku": "p5z343vdS",
-		  "upc": "939581000000",
-		  "pricePerUnit": "497.45",
-		  "quantityOnHand": 9703,
-		  "productName": "sticky note"
-		},
-		{
-		  "productId": 2,
-		  "manufacturer": "Hessel, Schimmel and Feeney",
-		  "sku": "i7v300kmx",
-		  "upc": "740979000000",
-		  "pricePerUnit": "282.29",
-		  "quantityOnHand": 9217,
-		  "productName": "leg warmers"
-		},
-		{
-		  "productId": 3,
-		  "manufacturer": "Swaniawski, Bartoletti and Bruen",
-		  "sku": "q0L657ys7",
-		  "upc": "111730000000",
-		  "pricePerUnit": "436.26",
-		  "quantityOnHand": 5905,
-		  "productName": "lamp shade"
-		}]`
-
-	err := json.Unmarshal([]byte(productListJSON), &productList)
+func main() {
+	product.SetupRoutes(basePath)
+	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func getNextID() int {
-	highestID := -1
-	for _, product := range productList {
-		if highestID < product.ProductID {
-			highestID = product.ProductID
-		}
-	}
-
-	return highestID + 1
-}
-
-func findProductByID(productID int) (*Product, int) {
-	for i, product := range productList {
-		if product.ProductID == productID {
-			return &product, i
-
-		}
-	}
-
-	return nil, 0
-}
-
-func productHandler(w http.ResponseWriter, r *http.Request) {
-	urlParameterSegment := strings.Split(r.URL.Path, "products/")
-	productID, err := strconv.Atoi(urlParameterSegment[len(urlParameterSegment)-1])
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	product, indexOfProduct := findProductByID(productID)
-	if product == nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	switch r.Method {
-	case http.MethodGet:
-		result, _ := json.Marshal(product)
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(result)
-	case http.MethodPut:
-		var updateProduct Product
-		body, err := ioutil.ReadAll(r.Body)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		err = json.Unmarshal(body, &updateProduct)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		if updateProduct.ProductID != productID {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		product = &updateProduct
-		productList[indexOfProduct] = *product
-		w.WriteHeader(http.StatusOK)
-		return
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-}
-
-func productsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		productsJSON, err := json.Marshal(&productList)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productsJSON)
-	case http.MethodPost:
-		var newProduct Product
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		err = json.Unmarshal(body, &newProduct)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if newProduct.ProductID != 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		newProduct.ProductID = getNextID()
-		productList = append(productList, newProduct)
-		w.WriteHeader(http.StatusCreated)
-		return
-	}
-}
-
-func middlewareHandler(handler http.HandlerFunc) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// before
-		handler.ServeHTTP(w, r)
-		// after
-	})
-}
-
-func main() {
-	productsHandleFunc := http.HandlerFunc(productsHandler)
-	producItemHandleFunc := http.HandlerFunc(productHandler)
-	http.Handle("/products", middlewareHandler(productsHandleFunc))
-	http.Handle("/products/", middlewareHandler(producItemHandleFunc))
-	http.ListenAndServe(":5000", nil)
 }
