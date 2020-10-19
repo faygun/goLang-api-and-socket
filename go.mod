01e436dd315a11b51e3249dde02387d65628e693
faygun <faygun89@gmail.com>
2020-10-19 20:12:00 +0100

    dependecies were added.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/faygun/goLang-api-and-socket
 
 go 1.12
+
+require github.com/go-sql-driver/mysql v1.5.0
--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/faygun/goLang-api-and-socket/database"
+
 	"github.com/faygun/goLang-api-and-socket/product"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const basePath = "/api"
 
 func main() {
+	database.SetupDatabase()
 	product.SetupRoutes(basePath)
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
